6958cf4f3801058bc828d3312c23e6438501e029
Bharat <bchotwani3@gmail.com>
2021-06-03 20:48:13 +0530

    docs

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#first-btn");
 var textTranslate = document.querySelector("#txt-box");
-var textOutput = document.querySelector("#txt-output")
+var textOutput = document.querySelector("#txt-output");
 
 // var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
@@ -8,12 +8,12 @@ var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
     return serverUrl + "?" + "text=" + text
-}
+};
 
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
-}
+};
 
 function clickHandler() {
     // textOutput.innerText = "bhbhbh " + textTranslate.value
