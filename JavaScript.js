6ff54373ca0ef7758f0bdfde0a4c2ce87c2c143e
Bharat <bchotwani3@gmail.com>
2021-06-03 13:16:46 +0530

    Import URL

--- JavaScript.js
+++ JavaScript.js
@@ -2,9 +2,27 @@ var btnTranslate = document.querySelector("#first-btn");
 var textTranslate = document.querySelector("#txt-box");
 var textOutput = document.querySelector("#txt-output")
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 function clickHandler() {
-    textOutput.innerText = "bhbhbh " + textTranslate.value
-    } 
+    // textOutput.innerText = "bhbhbh " + textTranslate.value
+    var inputText = textTranslate.value  // texting input
+
+    fetch(getTranslationURL(inputText))
+      .then (response => response.json())
+      .then (json => console.log(json.contents.translated))
+      .catch (errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickHandler)
